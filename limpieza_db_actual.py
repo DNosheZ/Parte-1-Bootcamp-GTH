c5bcc7d51992ff6fcfb26962ffe809414a1fbade
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2025-01-13 09:57:12 -0500

    v.1.4
    Cleaned 2024 actual employees database

--- limpieza_db_actual.py
+++ limpieza_db_actual.py
@@ -1,26 +1,47 @@
 import pandas as pd
 
 # Cargar el archivo
-df = pd.read_csv('Data_empleados_actuales_2024.csv')
+df1 = pd.read_csv('Data_empleados_actuales_2024.csv')
 
 # Renombrar columnas usando un diccionario
-df = df.drop(columns=['Unnamed: 0'])
+df1 = df1.drop(columns=['Unnamed: 0'])
+df1 = df1.drop(columns=['conciliacion'])
+df1 = df1.drop(columns=['anos_en_puesto'])
+df1 = df1.drop(columns=['id'])
+df1 = df1.drop(columns=['sexo'])
+df1['abandono'] = 'No'
+
 
 
 # Mostrar las dimensiones del dataset
-print("Dimensiones del dataset:", df.shape)
+#print("Dimensiones del dataset:", df.shape)
 
 # # Tipos de datos
 print("\nTipos de datos por columna:")
-print(df.dtypes)
+print(df1.dtypes)
 
 # # Valores faltantes
 
-df = df.drop(columns=['sexo'])
 print("\nValores faltantes por columna:")
-print(df.isnull().sum())
+print(df1.isnull().sum())
 # # Vista previa de los primeros registros
-print("\nPrimeros registros del dataset:")
-print(df.head())
+# print("\nPrimeros registros del dataset:")
+# print(df.head())
 # Exportar a un nuevo archivo
 # df.to_csv('Data_empleados_actuales_2024_clean.csv', index=False)
+
+df2 = pd.read_csv('Data_empleados_historicos_clean.csv')
+print("\nValores faltantes por columna:")
+print(df2.isnull().sum())
+
+# if df1.equals(df2):
+#     print("Las bases de datos son idénticas.")
+# else:
+#     print("Las bases de datos son diferentes.")
+#     columnas_exclusivas_df1 = set(df1.columns) - set(df2.columns)
+#     print("Columnas exclusivas de df1:", columnas_exclusivas_df1)
+
+#     # Columnas en df2 pero no en df1
+#     columnas_exclusivas_df2 = set(df2.columns) - set(df1.columns)
+#     print("Columnas exclusivas de df2:", columnas_exclusivas_df2)
+df1.to_csv('Data_empleados_actuales_2024_clean.csv', index=False)
